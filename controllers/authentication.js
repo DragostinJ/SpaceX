9a7380445a6fae91d117488a387ad4b44253f525
unknown <dragostinj@gmail.com>
2019-02-18 12:29:03 +0200

    finished auth routes

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,26 +1,39 @@
+const jwt = require("jwt-simple");
 const User = require("../mongoose/models/user");
-const jwt = require('jwt-simple');
-const config = require('../config');
+const config = require("../config");
 
 function tokenForUser(user) {
-  const timestamp = new Date().getTime()
-  return jwt.encode({sub: user.id, iat: timestamp}, config.secret)
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
-exports.signup = function(req, res, next) {
-  console.log(req.body);
+//Sign in
+exports.signin = function(req, res, next) {
+  //user had been auth with email pass
+  // we just need to give them a token
+  // req.user
+  // comes from the "done" callback from LocalStrategy-localLogin in passport
+  
+  res.send({ token: tokenForUser(req.user) })
+  //sends respond with token that is tokenForUser function that takes curr user
+};
 
+exports.signup = function(req, res, next) {
+  console.log(req.body)
   const email = req.body.email;
   const password = req.body.password;
 
-
   // TODO: more validation on the email !
-  if(!email || !password) {
-    return res.status(422).send({error: "you must provide email and password"})
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "you must provide email and password" });
   }
   // See if a user with the given email exist
   User.findOne({ email: email }, function(err, existingUser) {
-    if (err) { return next(err);}
+    if (err) {
+      return next(err);
+    }
     // If a user with email does not exist, return an erro
 
     if (existingUser) {
@@ -36,6 +49,6 @@ exports.signup = function(req, res, next) {
   user.save(function(err) {
     if (err) return next(err);
     // Respond to request indicating the user was created
-    res.json({token: tokenForUser(user)})
+    res.json({ token: tokenForUser(user) });
   });
 };
--- router.js
+++ router.js
@@ -4,14 +4,18 @@ const passport = require('passport');
 
 //interseptor/middleware
 
-const requireAuth = passport.authenticate('jwt', {session: false}) // no session coz we use cookie
+const requireAuth = passport.authenticate('jwt', {session: false}) // no session, we use cookie
+const requireSignin = passport.authenticate('local', {session: false})
 
 module.exports = function(app) {
     //change the path  later - we dont want every1 on the app to be logged
     app.get('/', requireAuth, function(req, res){
         res.send({hi: 'there'})
     })
-    app.post('/signup', Authentication.signup)
+    //auth the user before route they hit route handler
+    app.post('/signin', requireSignin, Authentication.signin)
+    app.post('/signup', Authentication.signup) 
+
 
 
 
