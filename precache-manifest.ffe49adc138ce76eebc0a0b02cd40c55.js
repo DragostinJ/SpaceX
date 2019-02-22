25bcd0e2077ad6ee4288d905f8c3b4ce708a6d2d
unknown <dragostinj@gmail.com>
2019-02-22 17:14:50 +0200

    Updates

---
+++ precache-manifest.ffe49adc138ce76eebc0a0b02cd40c55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "83493456dd5a89733bd1",
+    "url": "/spacex/static/js/runtime~main.83493456.js"
+  },
+  {
+    "revision": "6e2e9db5fff53acdb0ef",
+    "url": "/spacex/static/js/main.6e2e9db5.chunk.js"
+  },
+  {
+    "revision": "bf08376528ef49cbef32",
+    "url": "/spacex/static/js/1.bf083765.chunk.js"
+  },
+  {
+    "revision": "6e2e9db5fff53acdb0ef",
+    "url": "/spacex/static/css/main.18572635.chunk.css"
+  },
+  {
+    "revision": "d22924b357d9b27149317d40131e405d",
+    "url": "/spacex/index.html"
+  }
+];
