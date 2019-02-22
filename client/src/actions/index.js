f8608dcc13ee07e2579fac9e70c12ca31c31537d
unknown <dragostinj@gmail.com>
2019-02-22 17:36:27 +0200

    test221

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import { AUTH_USER, AUTH_ERROR } from "./types";
 export const signup = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post(
-      "http://localhost:5000/signup",
+      "/signup",
       formProps
     );
 
@@ -18,7 +18,7 @@ export const signup = (formProps, callback) => async dispatch => {
 export const signin = (formProps, callback) => async dispatch => {
   try {
     const response = await axios.post(
-      "http://localhost:5000/signin",
+      "/signin",
       formProps
     );
 
