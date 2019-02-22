69f4ea83444977fff0e43e34435f6d1bf8c4f3e1
unknown <dragostinj@gmail.com>
2019-02-22 16:36:32 +0200

    test10

--- server.js
+++ server.js
@@ -7,42 +7,26 @@ const schema = require("./schema");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-// const {buildSchema} = require('graphql')
+
 const morgan = require("morgan");
 const root = require("./rootValue");
 const http = require("http");
 const router = require("./router");
 
 
-//DB setup
-// setTimeout(function() {
-  //   mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
-// }, 60000);
-
-// mongoose.connect(("mongodb://127.0.0.1:27017/mongoPlay", { useNewUrlParser: true }, function(error) {
-  //  console.log(error)
-  // });
-
-// mongoose.connect("mongodb://127.0.0.1:27017/mongoPlay", { useNewUrlParser: true })
-// .then(
-  //   () => {  console.log("MongoDB is connected"); },
-//   err => { console.log("MongoDB connection unsuccessful, retry after 5 seconds.");
-//   //       setTimeout(connectWithRetry, 5000);}
-// )
-
-//   mongoose.connect("mongodb://127.0.0.1:27017/mongoPlay", { useNewUrlParser: true })
-
-// const db  = mongoose.connection
-// db.on('error', console.error.bind(console, 'connection error'))
-// db.once('once', function() {
-  //   console.log("MongoDB is connected");
-// })
-
-//App setup
 const connectWithRetry = () => {
   console.log("MongoDB connection with retry");
+  let mongoUserCredentials = '';
+  if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
+    mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
+  }
+
+  const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
+  const DB_NAME = process.env.MONGO_DB_NAME || 'mongoPlay';
+  const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
+
   mongoose
-    .connect("mongodb://localhost:27017/mongoPlay", { useNewUrlParser: true })
+    .connect(MONGO_CONNECTION_STRING, { useNewUrlParser: true })
     .then(() => {
       console.log("MongoDB is connected");
     })
@@ -52,11 +36,13 @@ const connectWithRetry = () => {
     });
 };
 connectWithRetry();
-
+ 
+// const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
+// const DB_NAME = process.env.MONGO_DB_NAME || 'sample-db';
 
 // App Setup
-
 const app = express();
+// app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(morgan("combined"));
 app.use(cors());
 app.use(bodyParser.json()); // to fix the types when i upload img ...
@@ -73,28 +59,10 @@ router(app);
 
 
 //Server Setup
-// const port = process.env.PORT || 3001
+
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server started on ${PORT}`));
 
-// require('./mongoose/config/db')
-
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://localhost/test');
-
-// const Cat = mongoose.model('Cat', {name: String})
-// let kitty = new Cat({name 'asdasd'})
-
-// mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true });
-// const db = mongoose.connection;
-// db.on('error', ()=> {console.log( '---FAILED to connect to mongoose')})
-// db.once('open', () => {
-//  console.log( '+++Connected to mongoose')
-// })
-
-// exports.test = function(req,res) {
-//   res.render('test');
-// };
